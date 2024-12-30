77e3b4fc41836a263986590d07a1cafc8a097bf2
UKKung <102902536+UKKung@users.noreply.github.com>
2024-12-30 15:28:56 +0700

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,14 @@
+Print ("k4zmm_'s Script is loading...")
+wait(0.5)
+_G.Mode = 'Normal'
+-- _G.Mode = 'something'
+local gameid = game.PlaceId
+
+if syn then request = syn.request end
+  if (gameid == 9912491609) and (_G.Mode == 'Normal') then
+    loadstring(request({['Url'] = 'https://raw.githubusercontent.com/UKKung/mainscript/refs/heads/main/fisch.lua', ['Method'] = 'GET'}).Body)()
+  elseif (gameid == 00000000) or (gameid == 123456789) and (_G.Mode == 'something') then
+    loadstring(request({['Url'] = 'www.google.com', ['Method'] = 'GET'}).Body)()
+  else
+    print('valid game')
+end
